e3817567719247fd2784e5bccec049e9d11ad794
Nicholas Offer <noffer@live.com>
2023-05-21 10:34:52 -0400

    links

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -6,59 +6,37 @@ function ProfilePage() {
       <h2 className="text-4xl text-white font-bold underline">Sample User</h2>
       {/* Profile Picture */}
       <div className="flex justify-center">
-          <img
-            src="https://waterlooblockchain.ca/_next/image?url=%2Fgoose.png&w=1920&q=75"
-            alt="Profile"
-            style={{ width: '100px', height: '100px' }}
-
-            className="w-100 h-100 mx-auto"
-          />
-        </div>
+        <img
+          src="https://waterlooblockchain.ca/_next/image?url=%2Fgoose.png&w=1920&q=75"
+          alt="Profile"
+          style={{ width: "100px", height: "100px" }}
+          className="w-100 h-100 mx-auto"
+        />
+      </div>
       {/* Biography */}
-      <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum. Sagittis purus sit amet volutpat. Amet commodo nulla facilisi nullam vehicula ipsum a arcu</p>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra
+        ipsum nunc aliquet bibendum. Sagittis purus sit amet volutpat. Amet
+        commodo nulla facilisi nullam vehicula ipsum a arcu
+      </p>
 
       {/* Links */}
-      <ul >
-        <li >
-          <a
-            href="link1"
-            
-            className="bg-lightBlue-500 text-white px-4 py-2 rounded-lg"    
-          >
-            Twitter
-          </a>
+      <ul>
+        <li className="bg-emerald-500 text-white px-4 py-2 my-5 rounded-lg text-center">
+          <a href="link1">Twitter</a>
         </li>
-        <li 
-            className="bg-lightBlue-500 text-white px-4 py-2 rounded-lg"> 
-          <a
-            href="link2"
-          >
-            DevPost
-          </a>
+        <li className="bg-emerald-500 text-white px-4 py-2 my-5 rounded-lg text-center">
+          <a href="link2">DevPost</a>
         </li>
-        <li 
-            className="bg-lightBlue-500 text-white px-4 py-2 rounded-lg">
-          <a
-            href="link3"
-          >
-            Instagram
-          </a>
+        <li className="bg-emerald-500 text-white px-4 py-2 my-5 rounded-lg text-center">
+          <a href="link3">Instagram</a>
         </li>
-        <li 
-            className="bg-lightBlue-500 text-white px-4 py-2 rounded-lg">
-          <a
-            href="link4"
-          >
-            LinkedIn
-          </a>
+        <li className="bg-emerald-500 text-white px-4 py-2 my-5 rounded-lg text-center">
+          <a href="link4">LinkedIn</a>
         </li>
-        <li 
-            className="bg-lightBlue-500 text-white px-4 py-2 rounded-lg">
-          <a
-            href="link5"
-          >
-            Wallet
-          </a>
+        <li className="bg-emerald-500 text-white px-4 py-2 my-5 rounded-lg text-center">
+          <a href="link5">Wallet</a>
         </li>
       </ul>
     </div>
