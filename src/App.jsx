bf2c6365534600e28f6615f5319baee831e65a3d
Usama Mohiuddin <usamamohiudin@gmail.com>
2023-05-22 09:31:29 -0400

    Final design for the OlympiHacks webpage

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import QrCode from "./QrCode";
 import "./styles.css"; //line 6-7 for qr code
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -40,35 +39,31 @@ function App() {
        <Navbar/>
       
       <header className="App-header">
-      <p>
-          Please Scan your Barcode
+      <p class="barcode_font">
+          Thank you for visiting JUNR
       </p>
-      </header>
+
+      <p className = "texter">"Welcome to our revolutionary barcode scanning platform powered by blockchain technology.
+At JUNR, we've combined the simplicity of barcode scanning with the security and transparency of blockchain to provide a seamless and trustworthy experience for accessing your digital wallets.
+With our cutting-edge barcode scanner, you can easily scan barcodes from physical items or digital screens, allowing you to effortlessly retrieve essential information and interact with your blockchain-based wallets.
+Experience the benefits of blockchain technology, including enhanced security, immutability, and decentralized control. Rest assured that your digital assets are protected by the power of the blockchain, providing you with peace of mind and confidence in your transactions.
+Whether you're a cryptocurrency enthusiast, a collector of digital assets, or simply looking for a secure way to manage your digital wallet, our platform is designed to meet your needs. We prioritize user privacy, data integrity, and seamless user experience.
+Join us today and unlock the power of blockchain-enabled barcode scanning. Take control of your digital assets with ease and explore a new level of convenience and security."
+Feel free to customize the text based on your specific offerings and branding.</p>
+
+    
+      <div className="card">
+        <button className="Button" onClick={() => setCount((count) => count + 1)}>
+          Access Wallet
+        </button>
+        
       </div>
       
+      <div className="barcode"><QrCode value={addr} /></div>
 
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <QrCode value={addr} />
+      </header>
         <Footer/>
+      </div>
     </>
   );
 }
